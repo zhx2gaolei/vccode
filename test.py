914f5afd6630c776d11f0611b656544e593523e1
unknown <45310811@qq.com>
2020-03-21 14:59:59 +0800

    commit

---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+
+ print "holle world"
+ for i in range(10):
+     print(i)
+print(sys.version)
---
+++ test1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test1.py
+@Time    :   2020/03/21 14:14:02
+@Author  :   DesignerA 
+@Version :   1.0
+@Contact :   DesignerA@qq.com
+@WebSite :   www.cnblogs.com/DesignerA
+'''
+# Start typing your code from here
